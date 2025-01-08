8b478144b9b722ebc0a4a431a860ad38a35ebdc1
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2025-01-08 23:27:51 +0530

    Get Applicants API + Data Display ✅

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -10,9 +10,12 @@ import {
 } from "../ui/table";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { MoreHorizontal } from "lucide-react";
+import { useSelector } from "react-redux";
 
 const shortlistingStatus = ["Accepted", "Rejected"];
 const ApplicantsTable = () => {
+  const { applicants } = useSelector((store) => store.application);
+
   return (
     <div>
       <Table>
@@ -28,29 +31,40 @@ const ApplicantsTable = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <tr>
-            <TableCell>FullName</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Contact Number</TableCell>
-            <TableCell>Resume</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell className="text-right cursor-pointer">
-              <Popover>
-                <PopoverTrigger>
-                  <MoreHorizontal />
-                </PopoverTrigger>
-                <PopoverContent className="w-32">
-                  {shortlistingStatus.map((status, index) => {
-                    return (
-                      <div key={index}>
-                        <span>{status}</span>
-                      </div>
-                    );
-                  })}
-                </PopoverContent>
-              </Popover>
-            </TableCell>
-          </tr>
+          {applicants &&
+            applicants?.applications?.map((application) => (
+              <tr key={application._id}>
+                <TableCell>{application?.applicant?.fullName}</TableCell>
+                <TableCell>{application?.applicant?.email}</TableCell>
+                <TableCell>{application?.applicant?.phoneNumber}</TableCell>
+                <TableCell className="text-blue-600 cursor-pointer">
+                  {" "}
+                  <a
+                    target="blank"
+                    href={application?.applicant?.profile?.resume}
+                  >
+                    {application?.applicant?.profile?.resumeOriginalName}
+                  </a>{" "}
+                </TableCell>
+                <TableCell>{application?.createdAt.split("T")[0]}</TableCell>
+                <TableCell className="text-right cursor-pointer">
+                  <Popover>
+                    <PopoverTrigger>
+                      <MoreHorizontal />
+                    </PopoverTrigger>
+                    <PopoverContent className="w-32">
+                      {shortlistingStatus.map((status, index) => {
+                        return (
+                          <div key={index}>
+                            <span>{status}</span>
+                          </div>
+                        );
+                      })}
+                    </PopoverContent>
+                  </Popover>
+                </TableCell>
+              </tr>
+            ))}
         </TableBody>
       </Table>
     </div>
