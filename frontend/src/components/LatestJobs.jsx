57015110d335079946bc8f57db60aee7e0435426
YourUsername <your-email@example.com>
2025-03-23 20:16:46 +0530

    Updated title ✅

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -8,7 +8,7 @@ const LatestJobs = () => {
   const { allJobs } = useSelector((store) => store.job);
   return (
     <div className="max-w-7xl mx-auto my-20">
-      <h1 className="text-4xl font-bold ">
+      <h1 className="text-4xl font-bold text-center ">
         {" "}
         <span className="text-[#6A38C2]">Latest</span> Job Openings
       </h1>
