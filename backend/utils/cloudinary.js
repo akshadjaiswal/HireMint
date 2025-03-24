78f53dc1415ea6e192b77ec5d708b9e95f05a867
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2025-03-24 22:26:15 +0530

    cloudinary check

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -2,6 +2,7 @@ import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 dotenv.config({});
 
+// Cloudinary Setup check 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
